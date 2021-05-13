ca096dcc893d87f36147ae4ad087fd592b9cf0b9
Dorota Tomala <doforever@gmail.com>
2021-05-13 15:26:56 +0200

    Move config and routes from server

--- server.js
+++ server.js
@@ -3,32 +3,14 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportConfig = require('./config/passport');
 
 const app = express();
-
-passport.use(new GoogleStrategy({
-  clientID: process.env.clientID,
-  clientSecret: process.env.clientSecret,
-  callbackURL: process.env.callbackURL,
-}, (accessToken, refreshToken, profile, done) => {
-  done(null, profile);
-}));
 
 app.use(session({ secret: process.env.sessionSecret }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -44,12 +26,6 @@ app.get('/', (req, res) => {
 
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
 
 app.get('/user/logged', (req, res) => {
   res.render('logged');
