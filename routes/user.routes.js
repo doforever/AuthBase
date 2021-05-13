2f1f4a5a79b650bd0ef7fa35704bceb8d2f151c5
Dorota Tomala <doforever@gmail.com>
2021-05-13 22:05:55 +0200

    Add user subpages accessible after authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
+const isLogged = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/user/no-permission');
+  } else {
+    next();
+  }
+};
+
+router.get('/logged', isLogged, (req, res) => {
   res.render('logged');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings');
+});
+
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile');
 });
 
 
